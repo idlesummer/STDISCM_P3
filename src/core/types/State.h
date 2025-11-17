910a95c233092cefa053cc49fbba8c5090705972
idlesummer <09louisthebob@gmail.com>
2025-11-18 04:06:02 +0800

    Deleted uneeded files

--- src/core/types/State.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-// Base state class - derive your game state from this
-class State {
-public:
-    virtual ~State() = default;
-    virtual State* clone() const = 0;  // For time-travel debugging
-};
